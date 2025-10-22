2a14267d02530d6e8a5f8d555726cac50354b018
Abhishek <abhisheksharma73341@gmail.com>
2025-10-22 09:25:25 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,10 +5,11 @@ function page() {
   return (
     <>
       <div className='links'>
-        <Link href={"/login"}>Admin Dashboard</Link>
-        <Link href={"/login"}>Faculty Dashboard</Link>
+        <Link href={"//dashboard/admin"}>Admin Dashboard</Link>
+        <Link href={"//dashboard/faculty"}>Faculty Dashboard</Link>
         <Link href={"/dashboard/hod"}>HOD Dashboard</Link>
         <Link href={"/dashboard/principal"}>Principal Dashboard</Link>
+        <Link href={"/login"}>Login Page</Link>
       </div>
     </>
   )
